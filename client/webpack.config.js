dc92f45870f2bea6b709e63437d857acd05fb08e
WJFil <greg.kitchen7@gmail.com>
2023-04-24 17:17:17 -0600

    fix package.json file for server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
 
-      // create manifest.json file.
+      // create manifest.json file.c
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
